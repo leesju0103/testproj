306396a1ea926b802ff6973b819551ddc20ad33d
seungju lee <dltmdwn0322@gmail.com>
2024-02-19 05:40:13 +0900

    #4 계산기 폼 수정

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -34,8 +34,8 @@
             붙여넣기ToolStripMenuItem = new ToolStripMenuItem();
             기록지우기ToolStripMenuItem = new ToolStripMenuItem();
             저장ToolStripMenuItem = new ToolStripMenuItem();
-            label_input = new Label();
             tableLayoutPanel1 = new TableLayoutPanel();
+            button_result = new Button();
             button_rac = new Button();
             button_point = new Button();
             button_zero = new Button();
@@ -47,15 +47,18 @@
             button_six = new Button();
             button_five = new Button();
             button_four = new Button();
-            button_percent = new Button();
+            button_mod = new Button();
             button_div = new Button();
             button_nine = new Button();
             button_eight = new Button();
             button_seven = new Button();
             button_add = new Button();
-            button_equal = new Button();
+            panel1 = new Panel();
+            label_input = new Label();
+            textbox_history = new RichTextBox();
             menuStrip1.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
@@ -98,19 +101,7 @@
             저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
             저장ToolStripMenuItem.Size = new Size(64, 29);
             저장ToolStripMenuItem.Text = "저장";
-            // 
-            // label_input
-            // 
-            label_input.BackColor = SystemColors.InactiveBorder;
-            label_input.BorderStyle = BorderStyle.FixedSingle;
-            label_input.Font = new Font("맑은 고딕", 16F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label_input.Location = new Point(24, 48);
-            label_input.Name = "label_input";
-            label_input.Padding = new Padding(0, 0, 0, 10);
-            label_input.Size = new Size(265, 200);
-            label_input.TabIndex = 1;
-            label_input.Text = "0";
-            label_input.TextAlign = ContentAlignment.BottomRight;
+            저장ToolStripMenuItem.Click += 저장ToolStripMenuItem_Click;
             // 
             // tableLayoutPanel1
             // 
@@ -120,7 +111,7 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            tableLayoutPanel1.Controls.Add(button_equal, 4, 2);
+            tableLayoutPanel1.Controls.Add(button_result, 4, 2);
             tableLayoutPanel1.Controls.Add(button_rac, 4, 1);
             tableLayoutPanel1.Controls.Add(button_point, 2, 3);
             tableLayoutPanel1.Controls.Add(button_zero, 0, 3);
@@ -132,7 +123,7 @@
             tableLayoutPanel1.Controls.Add(button_six, 2, 1);
             tableLayoutPanel1.Controls.Add(button_five, 1, 1);
             tableLayoutPanel1.Controls.Add(button_four, 0, 1);
-            tableLayoutPanel1.Controls.Add(button_percent, 4, 0);
+            tableLayoutPanel1.Controls.Add(button_mod, 4, 0);
             tableLayoutPanel1.Controls.Add(button_div, 3, 0);
             tableLayoutPanel1.Controls.Add(button_nine, 2, 0);
             tableLayoutPanel1.Controls.Add(button_eight, 1, 0);
@@ -148,6 +139,19 @@
             tableLayoutPanel1.Size = new Size(265, 186);
             tableLayoutPanel1.TabIndex = 2;
             // 
+            // button_result
+            // 
+            button_result.Dock = DockStyle.Fill;
+            button_result.Location = new Point(215, 95);
+            button_result.Margin = new Padding(3, 3, 0, 3);
+            button_result.Name = "button_result";
+            tableLayoutPanel1.SetRowSpan(button_result, 2);
+            button_result.Size = new Size(50, 88);
+            button_result.TabIndex = 21;
+            button_result.Text = "=";
+            button_result.UseVisualStyleBackColor = true;
+            button_result.Click += button_result_Click;
+            // 
             // button_rac
             // 
             button_rac.Dock = DockStyle.Fill;
@@ -158,6 +162,7 @@
             button_rac.TabIndex = 19;
             button_rac.Text = "1/x";
             button_rac.UseVisualStyleBackColor = true;
+            button_rac.Click += button_rac_Click;
             // 
             // button_point
             // 
@@ -169,6 +174,7 @@
             button_point.TabIndex = 17;
             button_point.Text = ".";
             button_point.UseVisualStyleBackColor = true;
+            button_point.Click += button_point_Click;
             // 
             // button_zero
             // 
@@ -181,6 +187,7 @@
             button_zero.TabIndex = 15;
             button_zero.Text = "0";
             button_zero.UseVisualStyleBackColor = true;
+            button_zero.Click += button_num_Click;
             // 
             // button_sub
             // 
@@ -192,6 +199,7 @@
             button_sub.TabIndex = 13;
             button_sub.Text = "-";
             button_sub.UseVisualStyleBackColor = true;
+            button_sub.Click += button_op_Click;
             // 
             // button_three
             // 
@@ -203,6 +211,7 @@
             button_three.TabIndex = 12;
             button_three.Text = "3";
             button_three.UseVisualStyleBackColor = true;
+            button_three.Click += button_num_Click;
             // 
             // button_two
             // 
@@ -214,6 +223,7 @@
             button_two.TabIndex = 11;
             button_two.Text = "2";
             button_two.UseVisualStyleBackColor = true;
+            button_two.Click += button_num_Click;
             // 
             // button_one
             // 
@@ -225,6 +235,7 @@
             button_one.TabIndex = 10;
             button_one.Text = "1";
             button_one.UseVisualStyleBackColor = true;
+            button_one.Click += button_num_Click;
             // 
             // button_mul
             // 
@@ -236,6 +247,7 @@
             button_mul.TabIndex = 8;
             button_mul.Text = "*";
             button_mul.UseVisualStyleBackColor = true;
+            button_mul.Click += button_op_Click;
             // 
             // button_six
             // 
@@ -247,6 +259,7 @@
             button_six.TabIndex = 7;
             button_six.Text = "6";
             button_six.UseVisualStyleBackColor = true;
+            button_six.Click += button_num_Click;
             // 
             // button_five
             // 
@@ -258,6 +271,7 @@
             button_five.TabIndex = 6;
             button_five.Text = "5";
             button_five.UseVisualStyleBackColor = true;
+            button_five.Click += button_num_Click;
             // 
             // button_four
             // 
@@ -269,17 +283,19 @@
             button_four.TabIndex = 5;
             button_four.Text = "4";
             button_four.UseVisualStyleBackColor = true;
+            button_four.Click += button_num_Click;
             // 
-            // button_percent
+            // button_mod
             // 
-            button_percent.Dock = DockStyle.Fill;
-            button_percent.Location = new Point(215, 3);
-            button_percent.Margin = new Padding(3, 3, 0, 3);
-            button_percent.Name = "button_percent";
-            button_percent.Size = new Size(50, 40);
-            button_percent.TabIndex = 4;
-            button_percent.Text = "%";
-            button_percent.UseVisualStyleBackColor = true;
+            button_mod.Dock = DockStyle.Fill;
+            button_mod.Location = new Point(215, 3);
+            button_mod.Margin = new Padding(3, 3, 0, 3);
+            button_mod.Name = "button_mod";
+            button_mod.Size = new Size(50, 40);
+            button_mod.TabIndex = 4;
+            button_mod.Text = "%";
+            button_mod.UseVisualStyleBackColor = true;
+            button_mod.Click += button_op_Click;
             // 
             // button_div
             // 
@@ -291,6 +307,7 @@
             button_div.TabIndex = 3;
             button_div.Text = "/";
             button_div.UseVisualStyleBackColor = true;
+            button_div.Click += button_op_Click;
             // 
             // button_nine
             // 
@@ -302,6 +319,7 @@
             button_nine.TabIndex = 2;
             button_nine.Text = "9";
             button_nine.UseVisualStyleBackColor = true;
+            button_nine.Click += button_num_Click;
             // 
             // button_eight
             // 
@@ -313,6 +331,7 @@
             button_eight.TabIndex = 1;
             button_eight.Text = "8";
             button_eight.UseVisualStyleBackColor = true;
+            button_eight.Click += button_num_Click;
             // 
             // button_seven
             // 
@@ -324,6 +343,7 @@
             button_seven.TabIndex = 0;
             button_seven.Text = "7";
             button_seven.UseVisualStyleBackColor = true;
+            button_seven.Click += button_num_Click;
             // 
             // button_add
             // 
@@ -335,26 +355,47 @@
             button_add.TabIndex = 20;
             button_add.Text = "+";
             button_add.UseVisualStyleBackColor = true;
+            button_add.Click += button_op_Click;
             // 
-            // button_equal
+            // panel1
             // 
-            button_equal.Dock = DockStyle.Fill;
-            button_equal.Location = new Point(215, 95);
-            button_equal.Margin = new Padding(3, 3, 0, 3);
-            button_equal.Name = "button_equal";
-            tableLayoutPanel1.SetRowSpan(button_equal, 2);
-            button_equal.Size = new Size(50, 88);
-            button_equal.TabIndex = 21;
-            button_equal.Text = "=";
-            button_equal.UseVisualStyleBackColor = true;
+            panel1.Controls.Add(label_input);
+            panel1.Controls.Add(textbox_history);
+            panel1.Location = new Point(24, 50);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(265, 195);
+            panel1.TabIndex = 3;
+            // 
+            // label_input
+            // 
+            label_input.BackColor = SystemColors.Window;
+            label_input.BorderStyle = BorderStyle.FixedSingle;
+            label_input.Font = new Font("맑은 고딕", 16F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label_input.Location = new Point(0, 142);
+            label_input.Name = "label_input";
+            label_input.Padding = new Padding(0, 0, 0, 10);
+            label_input.Size = new Size(266, 53);
+            label_input.TabIndex = 1;
+            label_input.Text = "0";
+            label_input.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // textbox_history
+            // 
+            textbox_history.BorderStyle = BorderStyle.FixedSingle;
+            textbox_history.Location = new Point(0, 0);
+            textbox_history.Name = "textbox_history";
+            textbox_history.RightToLeft = RightToLeft.Yes;
+            textbox_history.Size = new Size(265, 144);
+            textbox_history.TabIndex = 0;
+            textbox_history.Text = "";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(318, 480);
+            Controls.Add(panel1);
             Controls.Add(tableLayoutPanel1);
-            Controls.Add(label_input);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Form1";
@@ -362,6 +403,7 @@
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -374,7 +416,6 @@
         private ToolStripMenuItem 붙여넣기ToolStripMenuItem;
         private ToolStripMenuItem 기록지우기ToolStripMenuItem;
         private ToolStripMenuItem 저장ToolStripMenuItem;
-        private Label label_input;
         private TableLayoutPanel tableLayoutPanel1;
         private Button button_seven;
         private Button button20;
@@ -392,11 +433,14 @@
         private Button button_six;
         private Button button_five;
         private Button button_four;
-        private Button button_percent;
+        private Button button_mod;
         private Button button_div;
         private Button button_nine;
         private Button button_eight;
-        private Button button_equal;
+        private Button button_result;
         private Button button_add;
+        private Panel panel1;
+        private Label label_input;
+        private RichTextBox textbox_history;
     }
 }
